d7707d1e27279ec8ba9fbdb64eb6dae6b4af7da8
Corey <corey@undiwork>
2012-10-02 12:26:22 -0400

    Initial commit

---
+++ src/class_reader/core.clj
@@ -0,0 +1,1 @@
+(ns class-reader.core)
---
+++ test/class_reader/test/core.clj
@@ -0,0 +1,6 @@
+(ns class-reader.test.core
+  (:use [class-reader.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
